2aedf6d3d24c3b49e79bfc86800bea778601f895
Joachim <joachimvalgren@gmail.com>
2014-03-07 11:13:54 +0100

    TransferAmount

--- BankFrontendF/src/java/dk/cphbusiness/bank/view/frontController/TransferAmountCommand.java
+++ BankFrontendF/src/java/dk/cphbusiness/bank/view/frontController/TransferAmountCommand.java
@@ -1,6 +1,7 @@
 package dk.cphbusiness.bank.view.frontController;
 
 import dk.cphbusiness.bank.contract.BankManager;
+import dk.cphbusiness.bank.contract.dto.AccountDetail;
 import dk.cphbusiness.bank.contract.dto.AccountIdentifier;
 import dk.cphbusiness.bank.contract.dto.TransferSummary;
 import dk.cphbusiness.bank.contract.eto.InsufficientFundsException;
@@ -28,13 +29,14 @@ public class TransferAmountCommand extends TargetCommand {
         Double amount = Double.parseDouble(request.getParameter("amount"));
 
         try {
-            Collection<TransferSummary> transfers = manager.transferAmount(BigDecimal.valueOf(amount),
-                    accountNumber, targetAccountNumber).getTransfers();
+            AccountDetail account = manager.transferAmount(BigDecimal.valueOf(amount),
+                    accountNumber, targetAccountNumber);
+            Collection<TransferSummary> transfers = account.getTransfers();
             request.setAttribute("transfer", transfers);
             request.setAttribute("message", "Details for account number");
-            request.setAttribute("accountdetail", accountNumber);
+            request.setAttribute("accountdetail", account);
         } catch (InsufficientFundsException | NoSuchAccountException | TransferNotAcceptedException ex) {
-            
+
         }
         return super.execute(request);
     }
